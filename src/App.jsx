9d1db3477028178552390d03ad48bf96b453a12a
hamzanaeem03 <hamza04032003naeem@gmail.com>
2025-01-25 07:55:38 +0500

    added inital project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+
+  return (
+    <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+  </Routes>
+  )
+}
+
+export default App
